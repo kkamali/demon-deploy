d65d95ad72bc20938c65a47bc74d54c0dbfe9d68
Kathleen Kamali <kathleenkamali@gmail.com>
2020-05-03 08:13:38 -0700

    added basic untested routes for demons controller

--- demon-game-api/app/controllers/demons_controller.rb
+++ demon-game-api/app/controllers/demons_controller.rb
@@ -1,2 +1,11 @@
 class DemonsController < ApplicationController
+  def show
+    dialogue = Demon.find(params[:id]).dialogue
+    render json: dialogue
+  end
+
+  def index
+    demons = Demons.all
+    render json: demons
+  end
 end
